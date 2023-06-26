b2e976f1e7e7fdf72f5360e4b2bb28badb876134
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-26 15:31:10 +0900

    날짜 아카이브 뷰 로직 추가

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(verbose_name='TITLE', max_length=50)
@@ -17,4 +17,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=(self.slug,))
+
+    def get_previous(self):
+        return self.get_previous_by_modify_dt()
+
+    def get_next(self):
+        return self.get_next_by_modify_dt()
 
