9797e56ca83f6c61eaa4919aa30b9b61b7889d30
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-26 11:43:13 +0900

    bookmark url 연결

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -5,7 +5,7 @@ from bookmark.models import Bookmark
 # Register your models here.
 
 # 데코레이터 패턴을 사용하지 않는 방법
-# admin.site.regiter(Bookmark, BookmarkAdmin)
+# # admin.site.register(Bookmark, BookmarkAdmin)
 
 @admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
