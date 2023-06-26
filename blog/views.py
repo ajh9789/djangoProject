be6e487705d6a013db180ce9e547c2b069a648a9
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-06-26 16:22:47 +0900

    블로그 포스트 날짜(수정덜됨)

--- blog/views.py
+++ blog/views.py
@@ -12,24 +12,36 @@ class PostLV(ListView):
     context_object_name = 'posts'
     paginate_by = 2
 
+
 class PostDV(DetailView):
     model = Post
 
+
 class PostAV(ArchiveIndexView):
     model = Post
-    date_field = 'modify_dy'
+    date_field = 'modify_dt'
+
+
 class PostYAV(YearArchiveView):
     model = Post
     date_field = 'modify_dt'
     make_object_list = True
+    # month_format = '%b'
+
 
 class PostMAV(MonthArchiveView):
     model = Post
     date_field = 'modify_dt'
+    month_format = '%m'
+
+
 class PostDAV(DayArchiveView):
     model = Post
+    month_format = '%m'
     date_field = 'modify_dt'
+
 
 class PostTAV(TodayArchiveView):
     model = Post
     date_field = 'modify_dt'
+    template_name = 'blog/post_archive_day.html'
